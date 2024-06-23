ee443ad9a29d80a0fd30b8c3c8990311371ac866
lab-testt <saydhany012@gmail.com>
2024-06-23 23:15:23 +0300

    Update installDockerCompose.groovy

--- vars/installDockerCompose.groovy
+++ vars/installDockerCompose.groovy
@@ -1,17 +1,21 @@
-// File: vars/installDockerCompose.groovy
+// shared-library-repo/vars/installDockerCompose.groovy
 
-def call(body) {
-    // Stage code
-    stage('Install Docker Compose') {
-        steps {
-            sh '''
-                apk update
-                apk add --no-cache curl jq
-                COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)
-                curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-                chmod +x /usr/local/bin/docker-compose
-                docker-compose --version
-            '''
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Install Docker Compose') {
+                steps {
+                    sh '''
+                      apk update
+                      apk add --no-cache curl jq
+                      COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)
+                      curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                      chmod +x /usr/local/bin/docker-compose
+                      docker-compose --version
+                    '''
+                }
+            }
         }
     }
 }
