b7f309201e7931f30c000b6b3c344be539abb5fa
lab-testt <saydhany012@gmail.com>
2024-06-23 23:50:10 +0300

    Update and rename dockerLoginAndPush.groovy to dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,16 +1,14 @@
-// shared-library-repo/vars/dockerLoginAndPush.groovy
-
-def call(String dockerImage, String credentialsId) {
+def call() {
     pipeline {
         agent any
         stages {
             stage('Docker Login and Push') {
                 steps {
                     script {
-                        withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                        withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
                             sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
                         }
-                        docker.image(dockerImage).push()
+                        dockerImage.push('latest')
                     }
                 }
             }
