17b7dcca8cf0817798df00f33535e7682531164c
lab-testt <saydhany012@gmail.com>
2024-06-23 23:14:43 +0300

    Update dockerLoginAndPush.groovy

--- vars/dockerLoginAndPush.groovy
+++ vars/dockerLoginAndPush.groovy
@@ -1,22 +1,18 @@
-// File: vars/dockerLoginAndPush.groovy
+// shared-library-repo/vars/dockerLoginAndPush.groovy
 
-def call(body) {
-    // Default parameters
-    def params = [:]
-
-    // Evaluate the body block, allowing overrides of defaults
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = params
-    body()
-
-    // Stage code
-    stage('Docker Login and Push') {
-        steps {
-            script {
-                withCredentials([usernamePassword(credentialsId: params.credentialsId ?: 'dockerhub', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                    sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+def call(String dockerImage, String credentialsId) {
+    pipeline {
+        agent any
+        stages {
+            stage('Docker Login and Push') {
+                steps {
+                    script {
+                        withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                            sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+                        }
+                        docker.image(dockerImage).push()
+                    }
                 }
-                dockerImage.push('latest')
             }
         }
     }
